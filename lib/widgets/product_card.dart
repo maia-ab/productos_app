a4ec151e05534d9a2680c3912964cdc957d6e1a2
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-02-13 21:10:02 -0300

    termine de estilizar product card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -15,6 +15,7 @@ class ProductCard extends StatelessWidget {
           children: [
             _BackgroundImage(),
             _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag())
           ],
         ),
       ),
@@ -30,6 +31,34 @@ class ProductCard extends StatelessWidget {
           ]);
 }
 
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: sized_box_for_whitespace
+    return Container(
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          color: Colors.cyan,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$103.99',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+        ),
+      ),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   const _ProductDetails({
     super.key,
@@ -37,11 +66,38 @@ class _ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Disco duro G',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text('Id del disco duro',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+                maxLines: 1),
+          ],
+        ),
+      ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+      color: Colors.cyan,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
 }
 
 class _BackgroundImage extends StatelessWidget {
