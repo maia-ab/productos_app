118717b17c926bafdb28c427cfc1505d879783b8
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-02-17 23:59:48 -0300

    agregue product screen

---
+++ lib/services/product_service.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/models/product.dart';
+
+class ProductService extends ChangeNotifier {
+  final String _baseUrl = 'flutter-varios-259c0-default-rtdb.firebaseio.com/';
+  final List<Product> products = [];
+}
