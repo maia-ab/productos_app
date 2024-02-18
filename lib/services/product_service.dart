70e88acd717148f1c9fe82676be56bea45b9f821
unknown <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-02-18 00:00:31 -0300

    agregue models y services

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -4,4 +4,5 @@ import 'package:productos_app/models/product.dart';
 class ProductService extends ChangeNotifier {
   final String _baseUrl = 'flutter-varios-259c0-default-rtdb.firebaseio.com/';
   final List<Product> products = [];
+  //123
 }
